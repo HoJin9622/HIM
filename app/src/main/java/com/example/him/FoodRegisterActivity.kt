4cb4162a8e0caa6c5514f3865660b949e93626bb
HoJin9622 <kiss0104040@gmail.com>
2020-11-18 20:32:15 +0900

    fix: string 부분 에러 해결

--- app/src/main/java/com/example/him/FoodRegisterActivity.kt
+++ app/src/main/java/com/example/him/FoodRegisterActivity.kt
@@ -3,9 +3,9 @@ package com.example.him
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Food_Register_Activity : AppCompatActivity() {
+class FoodRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food__register)
+        setContentView(R.layout.activity_food_register)
     }
 }
