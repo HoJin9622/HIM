60d45a0ce5aab65c86e432269e019846fcaf55b9
r0cd7b <tkdrbs1928@naver.com>
2020-11-18 21:26:51 +0900

    Changed activity_login.xml

--- app/src/main/java/com/example/him/LoginActivity.kt
+++ app/src/main/java/com/example/him/LoginActivity.kt
@@ -2,8 +2,6 @@ package com.example.him
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import com.example.him.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -16,18 +14,5 @@ class LoginActivity : AppCompatActivity() {
         setContentView(view)
         // View Binding 완료. 아래부터 작성.
 
-        binding.idEdit.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
-            }
-        })
     }
 }
