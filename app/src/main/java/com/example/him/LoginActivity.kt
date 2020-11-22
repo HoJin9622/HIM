e841a3ae34a0b8f8826aba49364abdddb7414bc3
HoJin9622 <kiss0104040@gmail.com>
2020-11-22 16:42:46 +0900

    feat: 로그인 API 연결

--- app/src/main/java/com/example/him/LoginActivity.kt
+++ app/src/main/java/com/example/him/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.him.databinding.ActivityLoginBinding
 import kotlinx.android.synthetic.main.activity_login.*
 import retrofit2.Call
@@ -21,24 +22,23 @@ class LoginActivity : AppCompatActivity() {
         // View Binding 완료. 아래부터 작성.
 
         signInButton.setOnClickListener { moveRegisterPage() }
-        loginButton.setOnClickListener { moveMainPage() }
-
-        val body = HashMap<String, String>()
-        body["id"] = "example1"
-        body["password"] = "123456"
-
-        loginHandler(body)
+        loginButton.setOnClickListener { loginHandler() }
     }
 
-    private fun loginHandler(body: HashMap<String, String>) {
+    private fun loginHandler() {
+        val body = HashMap<String, String>()
+        body["id"] = idEdit.text.toString()
+        body["password"] = passwordEdit.text.toString()
+
         RetrofitClient.instance.login(body).enqueue(object : Callback<LoginResponse> {
             override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
                 Log.d("Response", response.toString())
-                Log.d("Response", "로그인 성공")
-                Log.d("Response", response.body()?._id.toString())
-                Log.d("Response", response.body()?.name.toString())
-                Log.d("Response", response.body()?.id.toString())
-                Log.d("Response", response.body()?.isProvider.toString())
+                Log.d("Response", "_id: " + response.body()?._id.toString())
+                Log.d("Response", "name: " + response.body()?.name.toString())
+                Log.d("Response", "id: " + response.body()?.id.toString())
+                Log.d("Response", "isProvider: " + response.body()?.isProvider.toString())
+
+                moveMainPage()
             }
 
             override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
