77ce62e8a7643c03d93dcc895e5b9b3a45616a7b
r0cd7b <tkdrbs1928@naver.com>
2020-11-23 21:08:44 +0900

    레이아웃 리스트 작성 중.

--- app/src/main/java/com/example/him/RegisterIngredientActivity.kt
+++ app/src/main/java/com/example/him/RegisterIngredientActivity.kt
@@ -1,11 +1,15 @@
 package com.example.him
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.example.him.databinding.ActivityEditIngredientBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,6 +19,8 @@ import kotlin.collections.HashMap
 class RegisterIngredientActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditIngredientBinding
 
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditIngredientBinding.inflate(layoutInflater)
@@ -22,16 +28,17 @@ class RegisterIngredientActivity : AppCompatActivity() {
         setContentView(view)
         // View Binding 완료. 아래부터 작성.
 
-        binding.confirmButton.setOnClickListener { registerIngredientHandler() }
+        binding.confirmButton.setOnClickListener { registerIngredientHandler(intent.getStringExtra("userId")) }
 
-        var dialog = DatePickerDialog(this)
-        binding.shelfLifeEdit.setOnFocusChangeListener { v, hasFocus ->
+        val dialog = DatePickerDialog(this)
+        binding.shelfLifeEdit.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus) {
                 dialog.show()
             }
         }
-        dialog.setOnDateSetListener { view, year, month, dayOfMonth ->
-            binding.shelfLifeEdit.setText("${year}-${month}-${dayOfMonth}")
+        dialog.setOnDateSetListener { _, year, month, dayOfMonth ->
+            binding.shelfLifeEdit.text =
+                Editable.Factory.getInstance().newEditable("${year}-${month + 1}-${dayOfMonth}")
         }
     }
 
@@ -39,12 +46,12 @@ class RegisterIngredientActivity : AppCompatActivity() {
         val body = HashMap<String, Any?>()
         body["user"] = userId
         body["name"] = binding.nameEdit.text.toString()
+        body["expirationDate"] = binding.shelfLifeEdit.text.toString()
         body["price"] = binding.priceEdit.text.toString().toInt()
         body["image"] = ""
         body["category"] = binding.categoryEdit.text.toString()
         body["memo"] = binding.memoEdit.text.toString()
         body["barcode"] = ""
-        body["expirationDate"] = binding.shelfLifeEdit.text.toString()
 
         RetrofitClient.instance.registerIngredient(body)
             .enqueue(object : Callback<IngredientResponse> {
@@ -57,6 +64,7 @@ class RegisterIngredientActivity : AppCompatActivity() {
                         Log.d("Response", "_id: " + response.body()?._id.toString())
                         Log.d("Response", "user: " + response.body()?.user.toString())
                         Log.d("Response", "name: " + response.body()?.name.toString())
+                        Log.d("Response", "expirationDate: ${response.body()?.expirationDate}")
                         Log.d("Response", "image: " + response.body()?.image.toString())
                         Log.d("Response", "category: " + response.body()?.category.toString())
                         Log.d("Response", "memo: " + response.body()?.memo.toString())
