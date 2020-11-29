c5848fb8f32c926524a3f282b763f491d2e6a56a
HoJin9622 <kiss0104040@gmail.com>
2020-11-29 23:46:48 +0900

    feat: 공급자 목록 fetching API

--- app/src/main/java/com/example/him/RegisterOrderActivity.kt
+++ app/src/main/java/com/example/him/RegisterOrderActivity.kt
@@ -2,7 +2,15 @@ package com.example.him
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.him.databinding.ActivityMainBinding
 import com.example.him.databinding.ActivityRegisterOrderBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.ArrayList
 
 class RegisterOrderActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterOrderBinding
@@ -13,5 +21,29 @@ class RegisterOrderActivity : AppCompatActivity() {
         var view = binding.root
         setContentView(R.layout.activity_register_order)
         // View Binding 완료. 아래부터 작성.
+
+        fetchProvider()
+    }
+
+    private fun fetchProvider() {
+        RetrofitClient.instance.getProvider()
+            .enqueue(object : Callback<ArrayList<UserResponse>> {
+                override fun onResponse(
+                    call: Call<ArrayList<UserResponse>>,
+                    response: Response<ArrayList<UserResponse>>
+                ) {
+                    // val responseCode = response.code().toString()
+                    Log.d("Response", "공급자 목록: " + response.body().toString())
+                }
+
+                override fun onFailure(call: Call<ArrayList<UserResponse>>, t: Throwable) {
+                    Log.d("Response", t.message.toString())
+                    Toast.makeText(
+                        this@RegisterOrderActivity,
+                        "서버와의 접속이 원활하지 않습니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
     }
 }
