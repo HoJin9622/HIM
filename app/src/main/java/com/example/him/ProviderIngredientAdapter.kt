4a3811690a7299f8c3a6f403fd8eba717dfb8112
r0cd7b <tkdrbs1928@naver.com>
2020-12-02 22:19:42 +0900

    필요 없는 import code를 제거했다.

--- app/src/main/java/com/example/him/ProviderIngredientAdapter.kt
+++ app/src/main/java/com/example/him/ProviderIngredientAdapter.kt
@@ -5,8 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.him.databinding.ProviderIngredientRecyclerBinding
 import java.text.SimpleDateFormat
