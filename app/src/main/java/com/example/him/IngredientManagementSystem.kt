b86aef374f44be3e51a4b584aceb7e8410eab9b7
r0cd7b <tkdrbs1928@naver.com>
2020-12-03 14:19:53 +0900

    Annotated partially.

--- app/src/main/java/com/example/him/IngredientManagementSystem.kt
+++ app/src/main/java/com/example/him/IngredientManagementSystem.kt
@@ -16,15 +16,20 @@ import kotlin.collections.HashMap
 class IngredientManagementSystem {
     // 식재료 목록을 보여주는 함수.
     fun show(activity: MainActivity, binding: ActivityMainBinding, userId: String?) {
+        // HTTP GET 명령으로 AWS 서버에서 접속된 사용자 ID에 해당하는 식재료 목록을 불러오는 함수.
         RetrofitClient.instance.getIngredients(userId)
             .enqueue(object : Callback<ArrayList<IngredientResponse>> {
+                // HTTP GET에 대한 정상적인 응답을 수신할 때 수행하는 함수.
                 override fun onResponse(
                     call: Call<ArrayList<IngredientResponse>>,
                     response: Response<ArrayList<IngredientResponse>>
                 ) {
                     Log.d("Response", "식재료 목록: ${response.body()}")
+                    // 수신된 정보(식재료 목록)을 List에 담는다.
                     val ingredientList: ArrayList<IngredientResponse>? = response.body()
+                    // 식재료 목록 UI를 동적으로 화면에 출력하기 위한 Adapter 클래스 생성.
                     val adapter = IngredientAdapter(activity)
+                    // 동적 UI인 Recycler View에 위 Adapter를 부착한다.
                     if (ingredientList != null) {
                         adapter.listIngredient = ArrayList(ingredientList)
                         binding.recyclerView.adapter = adapter
@@ -34,7 +39,7 @@ class IngredientManagementSystem {
                     }
                 }
 
-
+                // HTTP GET에 대한 응답이 실패될 때 수행하는 함수.
                 override fun onFailure(call: Call<ArrayList<IngredientResponse>>, t: Throwable) {
                     Log.d("Response", t.message.toString())
                     Toast.makeText(activity, "서버와의 접속이 원활하지 않습니다.", Toast.LENGTH_SHORT).show()
@@ -44,8 +49,10 @@ class IngredientManagementSystem {
 
     // 식재료를 등록하는 함수.
     fun register(activity: IngredientActivity, body: HashMap<String, Any?>) {
+        // HTTP POST 명령으로 사용자가 입력한 식재료 정보를 AWS 서버에 전송하는 함수.
         RetrofitClient.instance.registerIngredient(body)
             .enqueue(object : Callback<IngredientResponse> {
+                // 서버로부터 정상적인 응답이 수신되었을 때 수행하는 함수.
                 override fun onResponse(
                     call: Call<IngredientResponse>, response: Response<IngredientResponse>
                 ) {
@@ -57,6 +64,7 @@ class IngredientManagementSystem {
                         Log.d("Response", "expirationDate: ${response.body()?.expirationDate}")
                         Log.d("Response", "image: ${response.body()?.image}")
                         Log.d("Response", "barcode: ${response.body()?.barcode}")
+                        // 식재료 등록 화면을 닫는다.
                         activity.finish()
                     } else {
                         Log.d("Response", "response.code(): ${response.code()}")
@@ -64,6 +72,7 @@ class IngredientManagementSystem {
                     }
                 }
 
+                // 서버로부터 응답이 실패되었을 때 수행하는 함수.
                 override fun onFailure(call: Call<IngredientResponse>, t: Throwable) {
                     Log.d("Response", t.message.toString())
                     Toast.makeText(activity, "서버와의 접속이 원활하지 않습니다.", Toast.LENGTH_SHORT).show()
@@ -73,7 +82,9 @@ class IngredientManagementSystem {
 
     // 식재료를 수정하는 함수.
     fun edit(activity: IngredientActivity, body: HashMap<String, Any?>) {
+        // HTTP PUT 명령으로 사용자가 입력한 식재료 정보를 AWS 서버에 전송하는 함수.
         RetrofitClient.instance.editIngredient(body).enqueue(object : Callback<IngredientResponse> {
+            // 서버로부터 정상적인 응답이 수신되었을 때 수행하는 함수.
             override fun onResponse(
                 call: Call<IngredientResponse>, response: Response<IngredientResponse>
             ) {
@@ -85,6 +96,7 @@ class IngredientManagementSystem {
                     Log.d("Response", "expirationDate: ${response.body()?.expirationDate}")
                     Log.d("Response", "image: ${response.body()?.image}")
                     Log.d("Response", "barcode: ${response.body()?.barcode}")
+                    // 식재료 수정 화면을 닫는다.
                     activity.finish()
                 } else {
                     Log.d("Response", "response.code(): ${response.code()}")
@@ -92,6 +104,7 @@ class IngredientManagementSystem {
                 }
             }
 
+            // 서버로부터 응답이 실패되었을 때 수행하는 함수.
             override fun onFailure(call: Call<IngredientResponse>, t: Throwable) {
                 Log.d("Response", t.message.toString())
                 Toast.makeText(activity, "서버와의 접속이 원활하지 않습니다.", Toast.LENGTH_SHORT).show()
@@ -101,8 +114,10 @@ class IngredientManagementSystem {
 
     // 식재료를 삭제하는 함수.
     fun delete(activity: MainActivity, ingredientId: String) {
+        // HTTP DELETE 명령으로 사용자가 입력한 식재료 정보를 AWS 서버에 전송하는 함수.
         RetrofitClient.instance.deleteIngredients(ingredientId)
             .enqueue(object : Callback<MessageResponse> {
+                // 서버로부터 정상적인 응답이 수신되었을 때 수행하는 함수.
                 override fun onResponse(
                     call: Call<MessageResponse>,
                     response: Response<MessageResponse>
@@ -120,6 +135,7 @@ class IngredientManagementSystem {
                     }
                 }
 
+                // 서버로부터 응답이 실패되었을 때 수행하는 함수.
                 override fun onFailure(call: Call<MessageResponse>, t: Throwable) {
                     Log.d("Response", t.message.toString())
                     Toast.makeText(activity, "서버와의 접속이 원활하지 않습니다.", Toast.LENGTH_SHORT).show()
@@ -133,21 +149,25 @@ class IngredientManagementSystem {
         binding: ActivityIngredientBinding,
         body: HashMap<String, String?>
     ) {
-
+        // HTTP GET 명령으로 사용자가 스캔한 Barcode에 해당하는 식재료 정보를 AWS 서버에서 불러오는 함수.
         RetrofitClient.instance.getIngredient(body)
             .enqueue(object : Callback<IngredientResponse> {
+                // 서버로부터 정상적인 응답이 수신되었을 때 수행하는 함수.
                 override fun onResponse(
                     call: Call<IngredientResponse>, response: Response<IngredientResponse>
                 ) {
                     Log.d("Response", "결과: ${response.body()}")
                     if (response.body() != null) {
+                        // 불러온 식재료 정보를 UI에 입력한다.
                         binding.nameEdit.setText(response.body()!!.name)
                         binding.priceEdit.setText(response.body()!!.price.toString())
+                        // 불러온 식재료의 URL를 이용하여 해당 사진을 적재하는 함수.
                         Glide.with(binding.photoButton.context).load(response.body()?.image)
                             .into(binding.photoButton)
                     }
                 }
 
+                // 서버로부터 응답이 실패되었을 때 수행하는 함수.
                 override fun onFailure(call: Call<IngredientResponse>, t: Throwable) {
                     Log.d("Response", t.message.toString())
                     Toast.makeText(activity, "서버와의 접속이 원활하지 않습니다.", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/him/OrderManagementSystem.kt
+++ app/src/main/java/com/example/him/OrderManagementSystem.kt
@@ -17,7 +17,9 @@ import retrofit2.Response
 class OrderManagementSystem {
     // 현재 접속한 사용자가 공급자인지 소비자인지 검사하는 함수.
     fun isProvider(activity: OrderListActivity, binding: ActivityOrderListBinding, userId: String) {
+        // HTTP GET 명령으로 AWS 서버에서 접속된 사용자 ID에 해당하는 가입 정보(공급자 or 소비자)를 불러오는 함수.
         RetrofitClient.instance.isProvider(userId).enqueue(object : Callback<IsProviderResponse> {
+            // HTTP GET에 대한 정상적인 응답을 수신할 때 수행하는 함수.
             override fun onResponse(
                 call: Call<IsProviderResponse>,
                 response: Response<IsProviderResponse>
@@ -28,12 +30,14 @@ class OrderManagementSystem {
                     null -> {
                         Toast.makeText(activity, "서버와의 접속이 원활하지 않습니다.", Toast.LENGTH_SHORT).show()
                     }
+                    // 해당 사용자의 ID가 공급자일 경우 식재료 주문 버튼을 숨긴다.
                     true -> {
                         binding.registerOrderButton.hide()
                     }
                 }
             }
 
+            // HTTP GET에 대한 응답이 실패될 때 수행하는 함수.
             override fun onFailure(call: Call<IsProviderResponse>, t: Throwable) {
                 Log.d("Response", t.message.toString())
                 Toast.makeText(activity, "서버와의 접속이 원활하지 않습니다.", Toast.LENGTH_SHORT).show()
@@ -43,15 +47,20 @@ class OrderManagementSystem {
 
     // 현재 접속한 사용자의 식재료 주문 목록을 보여주는 합수.
     fun show(activity: OrderListActivity, binding: ActivityOrderListBinding, userId: String) {
+        // HTTP GET 명령으로 AWS 서버에서 접속된 사용자 ID에 해당하는 주문 목록을 불러오는 함수.
         RetrofitClient.instance.getOrders(userId)
             .enqueue(object : Callback<ArrayList<OrderResponse>> {
+                // HTTP GET에 대한 정상적인 응답을 수신할 때 수행하는 함수.
                 override fun onResponse(
                     call: Call<ArrayList<OrderResponse>>,
                     response: Response<ArrayList<OrderResponse>>
                 ) {
                     Log.d("Response", "주문 목록: ${response.body()}")
+                    // 수신된 정보(주문 목록)을 List에 담는다.
                     val orderList = response.body()
+                    // 주문 목록 UI를 동적으로 화면에 출력하기 위한 Adapter 클래스 생성.
                     val adapter = OrderAdapter(activity)
+                    // 동적 UI인 Recycler View에 위 Adapter를 부착한다.
                     if (orderList != null) {
                         adapter.listOrder = ArrayList(orderList)
                         binding.recyclerView.adapter = adapter
@@ -62,6 +71,7 @@ class OrderManagementSystem {
                     }
                 }
 
+                // HTTP GET에 대한 응답이 실패될 때 수행하는 함수.
                 override fun onFailure(call: Call<ArrayList<OrderResponse>>, t: Throwable) {
                     Log.d("Response", t.message.toString())
                     Toast.makeText(activity, "서버와의 접속이 원활하지 않습니다.", Toast.LENGTH_SHORT).show()
@@ -71,16 +81,20 @@ class OrderManagementSystem {
 
     // 현재 접속 사용자의 식재료를 삭제하는 함수.
     fun delete(activity: OrderListActivity, orderId: String) {
+        // HTTP DELETE 명령으로 AWS 서버에서 접속된 사용자 ID에 해당하는 주문을 삭제하는 함수.
         RetrofitClient.instance.deleteOrder(orderId)
             .enqueue(object : Callback<MessageResponse> {
+                // HTTP DELETE에 대한 정상적인 응답을 수신할 때 수행하는 함수.
                 override fun onResponse(
                     call: Call<MessageResponse>, response: Response<MessageResponse>
                 ) {
                     Log.d("Response", "결과: $response")
+                    // 주문 목록 화면에서 전체 주문 목록 UI를 갱신하여 출력하는 함수.
                     activity.showOrders()
                     Toast.makeText(activity, "해당 주문이 취소되었습니다.", Toast.LENGTH_SHORT).show()
                 }
 
+                // HTTP DELETE에 대한 응답이 실패될 때 수행하는 함수.
                 override fun onFailure(call: Call<MessageResponse>, t: Throwable) {
                     Log.d("Response", t.message.toString())
                     Toast.makeText(activity, "서버와의 접속이 원활하지 않습니다.", Toast.LENGTH_SHORT).show()
@@ -90,8 +104,10 @@ class OrderManagementSystem {
 
     // 식재료를 주문하기 전 공급자 목록을 불러오는 함수.
     fun bringProvider(activity: RegisterOrderActivity, binding: ActivityRegisterOrderBinding) {
+        // HTTP GET 명령으로 AWS 서버에 등록된 공급자 목록을 불러오는 함수.
         RetrofitClient.instance.getProvider()
             .enqueue(object : Callback<java.util.ArrayList<UserResponse>> {
+                // HTTP GET에 대한 정상적인 응답을 수신할 때 수행하는 함수.
                 override fun onResponse(
                     call: Call<java.util.ArrayList<UserResponse>>,
                     response: Response<java.util.ArrayList<UserResponse>>
@@ -102,16 +118,21 @@ class OrderManagementSystem {
                         return
                     }
 
+                    // 수신된 정보(공급자 목록)을 List에 담는다.
                     val providerList = response.body()!!
+                    // Spinner의 첫번 째 항목에는 설명을 List에 담는다.
                     val providerNameList = mutableListOf("공급자를 선택하세요.")
+                    // Spinner의 나머지 항목은 공급자 정보 중 이름만 선별하여 List에 담는다.
                     providerNameList.addAll(providerList.map { it.name })
 
+                    // 공급자 목록 UI를 동적으로 화면에 출력하기 위한 Adapter 클래스 생성.
                     val adapter = ArrayAdapter(
                         activity, android.R.layout.simple_list_item_1, providerNameList
                     )
                     binding.providerSpinner.adapter = adapter
                     binding.providerSpinner.onItemSelectedListener =
                         object : AdapterView.OnItemSelectedListener {
+                            // Spinner에서 공급자가 선택되었을 때 수행되는 이벤트 함수.
                             override fun onItemSelected(
                                 parent: AdapterView<*>?, view: View?, position: Int, id: Long
                             ) {
@@ -120,9 +141,11 @@ class OrderManagementSystem {
                                 } else {
                                     null
                                 }
+                                // 선택된 공급자에 해당하는 식재료 목록을 불러오는 함수.
                                 showProviderIngredient(activity, binding, providerId)
                             }
 
+                            // Spinner에서 아무것도 선택되지 않았을 때 수행되는 이벤트 함수.
                             override fun onNothingSelected(parent: AdapterView<*>?) {
                                 Toast.makeText(activity, "공급자를 먼저 선택해주세요.", Toast.LENGTH_SHORT)
                                     .show()
@@ -130,6 +153,7 @@ class OrderManagementSystem {
                         }
                 }
 
+                // HTTP GET에 대한 응답이 실패될 때 수행하는 함수.
                 override fun onFailure(
                     call: Call<java.util.ArrayList<UserResponse>>, t: Throwable
                 ) {
@@ -145,6 +169,7 @@ class OrderManagementSystem {
         binding: ActivityRegisterOrderBinding,
         userId: String?
     ) {
+        // 선택된 공급자의 식재료가 없을 경우 식재료 목록 UI를 비운다.
         if (userId == null) {
             val adapter = ProviderIngredientAdapter(activity)
             adapter.listIngredient = emptyList()
@@ -153,16 +178,21 @@ class OrderManagementSystem {
             return
         }
 
+        // HTTP GET 명령으로 AWS 서버에 등록된 해당 공급자의 식재료 목록을 불러오는 함수.
         RetrofitClient.instance.getIngredients(userId)
+            // HTTP GET에 대한 정상적인 응답을 수신할 때 수행하는 함수.
             .enqueue(object : Callback<ArrayList<IngredientResponse>> {
                 override fun onResponse(
                     call: Call<ArrayList<IngredientResponse>>,
                     response: Response<ArrayList<IngredientResponse>>
                 ) {
                     Log.d("Response", "식재료 목록: ${response.body()}")
+                    // 수신된 정보(선택된 공급자의 식재료 목록)을 List에 담는다.
                     val ingredientList: ArrayList<IngredientResponse>? = response.body()
+                    // 식재료 목록 UI를 동적으로 화면에 출력하기 위한 Adapter 클래스 생성.
                     val adapter = ProviderIngredientAdapter(activity)
                     if (ingredientList != null) {
+                        // 식재료 목록 UI를 갱신한다.
                         adapter.listIngredient = ArrayList(ingredientList)
                         binding.recyclerView.adapter = adapter
                         binding.recyclerView.layoutManager = LinearLayoutManager(activity)
@@ -171,6 +201,7 @@ class OrderManagementSystem {
                     }
                 }
 
+                // HTTP GET에 대한 응답이 실패될 때 수행하는 함수.
                 override fun onFailure(
                     call: Call<ArrayList<IngredientResponse>>,
                     t: Throwable
